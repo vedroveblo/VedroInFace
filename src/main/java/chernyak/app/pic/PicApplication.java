d42755fe8eb984ad17cbe6b67cb1d0064e73bc65
defo <vedroveblo@yandex.ru>
2025-02-07 15:16:14 +0300

    0.0.1

---
+++ src/main/java/chernyak/app/pic/PicApplication.java
@@ -0,0 +1,11 @@
+package chernyak.app.pic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(PicApplication.class, args);
+	}
+}
---
+++ src/test/java/chernyak/app/pic/PicApplicationTests.java
@@ -0,0 +1,13 @@
+package chernyak.app.pic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
